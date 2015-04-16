a222dfd1853589d7f6044100a7d00e66820fe9e3
Beka Rice <beka.rice@gmail.com>
2015-04-16 16:05:14 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== WP Shortlink Toolbar ===
+Contributors: beka.rice
+Tags: shortlink, toolbar, admin toolbar
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=paypal@skyverge.com&item_name=Donation+for+WP+Shortlink+Toolbar
+Requires at least: 3.8
+Tested up to: 4.2
+Stable Tag: 1.0.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Adds a "Shortlinks" menu to the WP Toolbar so that you can easily get the shortlink while viewing a post or page (while logged in), or share it via Twitter or Buffer.
+
+== Changelog ==
+
+= 2015.04.01 - version 1.0.0 =
+ * Initial Release
---
+++ shortlink-toolbar.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: Shortlink Toolbar
+ * Plugin URI: http://www.bekarice.com/plugins/
+ * Description: Adds a menu to the admin toolbar to get the shortlink for a post or share the shortlink via Twitter, Buffer.
+ * Author: Beka Rice
+ * Author URI: http://www.bekarice.com/
+ * Version: 1.0.0
+ * Text Domain: shortlink-toolbar
+ *
+ * Copyright: (c) 2015 Yoast and 2015 Beka Rice
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * This plugin is a derivative work of the Bit.ly Shortlinks plugin
+ * Credit: Joost de Valk
+ * http://yoast.com/wordpress/bitly-shortlinks/
+ *
+ * @package   Shortlink-Toolbar
+ * @author    Beka Rice
+ * @category  Admin
+ * @copyright Copyright (c) 2015 Yoast, 2015 Beka Rice
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ *
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Plugin Description
+ *
+ * Adds a menu to the admin toolbar to get the shortlink for a post; 
+ * if wp.me shortlinks are enabled via Jetpack, this will be used instead
+ *
+ */
+
+function shortlink_toolbar_menu() {
+
+	global $wp_admin_bar, $post;
+
+	if ( !isset( $post->ID ) ) {
+		return;
+	}
+		
+	$short = wp_get_shortlink( $post->ID, 'query' );
+	
+	$twitter_link = 'https://twitter.com/intent/tweet?text=' . str_replace('+','%20', urlencode( $post->post_title . ' - '. $short ) ) . '&source=webclient';
+	
+	$buffer_link = 'https://bufferapp.com/add?url="' . $short . '&source=admin&text=' . str_replace('+','%20', urlencode( $post->post_title . ' - '. $short ) );
+	
+	if ( is_singular() && !is_preview() ) {
+
+		$wp_admin_bar->add_node( array( 'id' => 'shortlink', 'title' => __( 'Shortlinks', 'shortlink-toolbar' ), 'href' => '#', 'meta' => array( 'onclick' => 'javascript:prompt("Short Link:", "' . $short . '");' ) ) );
+		$wp_admin_bar->add_node( array( 'parent' => 'shortlink', 'id' => 'shortlink_shortened-link', 'title' => __( 'Get Shortlink', 'shortlink-toolbar' ), 'href' => '#', 'meta' => array( 'onclick' => 'javascript:prompt("Short Link:", "' . $short . '");' ) ) );
+		$wp_admin_bar->add_node( array( 'parent' => 'shortlink', 'id' => 'shortlink_twitterlink', 'title' => __( 'Share on Twitter', 'shortlink-toolbar' ), 'href' => $twitter_link, 'meta' => array( 'target' => '_blank' ) ) );
+		$wp_admin_bar->add_node( array( 'parent' => 'shortlink', 'id' => 'shortlink_bufferlink', 'title' => __( 'Share via Buffer', 'shortlink-toolbar' ), 'href' => $buffer_link,  'meta' => array( 'target' => '_blank' ) ) );
+	}
+}
+add_action( 'admin_bar_menu', 'shortlink_toolbar_menu', 95 );
